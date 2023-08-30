7b26a6acb13a3518931fa218ba3833de10d3c3a8
thawanrat43 <129850059+thawanrat43@users.noreply.github.com>
2023-08-30 14:07:35 +0700

    update

--- app.js
+++ app.js
@@ -31,8 +31,12 @@ const upload = multer({dest: 'uploads/'});
 app.use(cookieParser());
 // parse application/json
 // app.use(cors())
-
-app.use(cors());
+const corsOptions ={
+    origin:'https://front-end-updatenew-b558.vercel.app', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 // app.use((req,res,next)=>{
 //   res.header('Access-Control-Allow-Origin','*');
 //   res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
