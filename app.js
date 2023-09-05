6aad7e1ff2ff8fa95c94e0c3d6847fa50f61a29d
thawanrat43 <129850059+thawanrat43@users.noreply.github.com>
2023-09-05 15:10:11 +0700

    update

--- app.js
+++ app.js
@@ -29,21 +29,21 @@ const upload = multer({dest: 'uploads/'});
 // create application/json parser
 app.use(cookieParser());
 // parse application/json
-const whitelist = ['http://localhost:3000', 'https://lambent-donut-b06776.netlify.app'];
+// const whitelist = ['http://localhost:3000', 'https://lambent-donut-b06776.netlify.app'];
 
-var corsOptions = {
-  credentials: true,
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// var corsOptions = {
+//   credentials: true,
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
-app.use(cors(corsOptions));
-
+// app.use(cors(corsOptions));
+app.use(cors());
 // const API_URL ='http://localhost:3000'
 // const proxyOptions = {
 //   target: API_URL,
