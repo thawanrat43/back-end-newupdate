33420328cd96d4c40b564e34edac9f96e26947c0
thawanrat43 <129850059+thawanrat43@users.noreply.github.com>
2023-09-06 11:47:22 +0700

    update

--- app.js
+++ app.js
@@ -186,18 +186,18 @@ app.post('/login', function (req, res, next) {
               // session.userid = users[0].id;
               // req.session.islogin = true;
               // console.log(req.session)
-              const token = jwt.sign({ id :users[0].id},process.env.TOKEN_KEY,{expiresIn:"2h"});
-              // // users[0].token = token;
-              // // return res.status(201).json(users);
-              // return res
-              //   .cookie("access_token", token, {
-              //     httpOnly: true,
+              // const token = jwt.sign({ id :users[0].id},process.env.TOKEN_KEY,{expiresIn:"2h"});
+              // // // users[0].token = token;
+              // // // return res.status(201).json(users);
+              // // return res
+              // //   .cookie("access_token", token, {
+              // //     httpOnly: true,
                   
-              //   })
-              //   .status(200)
-              //   .json(users[0].id);
+              // //   })
+              // //   .status(200)
+              // //   .json(users[0].id);
                 
-              return res.json({status : 'OK',message:'login success',token})
+              // return res.json({status : 'OK',message:'login success',token})
             }else{
               return res.json({status : 'Error'})
             }
